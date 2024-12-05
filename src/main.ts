af9e407c180b6dd0f0e4441046a8636b44f64b02
Emiliano Nataren Del Rivero <eminataren2002@gmail.com>
2024-12-04 21:50:39 -0600

    feature: (Concurrency requests - monitor_manager)

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,44 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Worker } from 'worker_threads';
+
+function startWorker(path: string, port: number): Promise<void> {
+  return new Promise((resolve, reject) => {
+    console.log(`Starting worker on port ${port}...`);
+    const worker = new Worker(path, { workerData: { port } });
+
+    worker.on('message', (message) => {
+      console.log(`Worker on port ${port} message: ${message}`);
+      if (message === 'Worker started successfully') {
+        resolve();
+      }
+    });
+
+    worker.on('error', (error) => {
+      console.error(`Worker on port ${port} error: ${error.message}`);
+      reject(error);
+    });
+
+    worker.on('exit', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Worker on port ${port} stopped with exit code ${code}`));
+      } else {
+        console.log(`Worker on port ${port} exited successfully.`);
+      }
+    });
+  });
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('MedMonitor API')
-    .setDescription('helpfull right?')
-    .setVersion('1.0')
-    .addTag('medmonitor')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  app.setGlobalPrefix('api/v1')
-  app.enableCors({
-    origin: [process.env.FRONTEND_URL,'http://localhost:3000'],
-  })
-  app.useGlobalPipes(new ValidationPipe({  
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-    transformOptions: {
-      enableImplicitConversion: true
-    }
-  }))
-  await app.listen(4000);
+  try {
+    console.log('Starting workers...');
+  
+    await Promise.all([
+      startWorker(require.resolve('./worker'), 4000), 
+      startWorker(require.resolve('./worker'), 4001), 
+    ]);
+    console.log('All workers started successfully');
+  } catch (error) {
+    console.error(`Error starting workers: ${error.message}`);
+  }
 }
-bootstrap();
+
+bootstrap(); 
